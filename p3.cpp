7783368a08ec0315317fca71e1905c0e90d2d0f2
gchodges <garretthodges16@gmail.com>
2022-10-12 21:28:14 -0400

    Add initial files

---
+++ p3.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <string>
+
+#include "p3.h"
+
+using namespace std;
+
+Info::Info() {
+    this->name = "No Name Set";
+    this->value = "No Value Set";
+    this->next = nullptr;
+}
+
+Info::Info(std::string name, std::string value, Info *next) {
+    this->name = name;
+    this->value = value;
+    this->next = next;
+}
+
+Contact::Contact() {
+    this->first = "No First Set";
+    this->last = "No Last Set";
+    this->next = nullptr;
+    this->headInfoList = nullptr;
+}
+
+Contact::Contact(std::string first, std::string last, Contact *next) {
+    this->first = first;
+    this->last = last;
+    this->next = next;
+    this->headInfoList = nullptr;
+}
+
+ContactList::ContactList() {
+    this->headContactList = nullptr;
+    this->count = 0;
+}
+
+int ContactList::getCount() {
+    return this->count;
+}
+
+// print the specified contact and its information
+// 1. return false and print nothing if the contact is not in the list
+// 2. otherwise return true and print the contact
+bool ContactList::printContact(std::ostream &os, std::string first, std::string last) {
+    return true;
+}
+
+// print all contacts and their information
+// print nothing if the list is empty
+void ContactList::print(std::ostream &os) {
+    
+}
+
+// add a contact to the back of the list
+// 1. return false and do nothing if the contact is already in the list
+// 2. otherwise return true and add the contact to the back of the list
+// - do not forget to update count
+bool ContactList::addContact(std::string first, std::string last) {
+    Contact* newContact = new Contact;
+    newContact->first = first;
+    newContact->last = last;
+    newContact->next = nullptr;
+    if(this->checkList(newContact) == false){
+        if (headContactList-> next != nullptr){
+            Contact* temp = headContactList;
+
+            while(temp->next != nullptr){
+                temp = temp->next;
+            }
+            temp->next = newContact;
+        }
+        else{
+            headContactList->next = newContact;
+        }
+            this->count++;
+        
+            return true;
+    }
+    else return false;
+}
+
+bool ContactList::checkList(Contact* toCheck){
+    Contact* temp = new Contact;
+    temp = this->headContactList;
+
+    while(temp->next != nullptr){
+        if (temp->first == toCheck->first && temp->last == toCheck->last){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// add info to the back of a contact's info list
+// 1. return false and do nothing if the contact is not in the list
+// 2. if the infoName is already in the info list, update the infoValue and return true
+// 3. otherwise add the info to the back of the contact's list and return true
+bool ContactList::addInfo(std::string first, std::string last, std::string infoName, std::string infoVal) {
+    return true;
+}
+
+// add a contact to the list in ascending order by last name
+//     if last names are equal, then order by first name ascending
+// 1. return false and do nothing if the contact is already in the list
+// 2. otherwise return true and add the contact to the list
+// - do not forget to update count
+// - compare strings with the built-in comparison operators: <, >, ==, etc.
+// - a compare method/function is recommended
+bool ContactList::addContactOrdered(std::string first, std::string last) {
+    return true;
+}
+
+// add info to a contact's info list in ascending order by infoName
+// 1. return false and do nothing if the contact is not in the list
+// 2. if the infoName is already in the info list, update the infoValue and return true
+// 3. otherwise add the info to the contact's list and return true
+bool ContactList::addInfoOrdered(std::string first, std::string last, std::string infoName, std::string infoVal) {
+    return true;
+}
+
+// remove the contact and its info from the list
+// 1. return false and do nothing if the contact is not in the list
+// 2. otherwise return true and remove the contact and its info
+// - do not forget to update count
+bool ContactList::removeContact(std::string first, std::string last) {
+    return true;
+}
+
+// remove the info from a contact's info list
+// 1. return false and do nothing if the contact is not in the list
+// 2. return false and do nothing if the info is not in the contact's info list
+// 3. otherwise return true and remove the info from the contact's list
+bool ContactList::removeInfo(std::string first, std::string last, std::string infoName) {
+    return true;
+}
+
+// destroy the list by removing all contacts and their infos
+ContactList::~ContactList() {
+
+}
+
+// deep copy the source list
+// - do not forget to update count
+ContactList::ContactList(const ContactList &src) {
+
+}
+
+// remove all contacts and their info then deep copy the source list
+// - do not forget to update count
+const ContactList &ContactList::operator=(const ContactList &src) {
+    if (this != &src) {
+
+    }
+    return *this;
+}
